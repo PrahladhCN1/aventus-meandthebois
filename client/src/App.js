41b45fc5e6132dc9a0e9fddc0e654e2c0e02f5a7
Niranjan S <111201410+Ninja2k3@users.noreply.github.com>
2023-06-01 17:19:21 +0530

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from './components/Login';
+import Topbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Topbar/>
       <Login/>
     </div>
   );
