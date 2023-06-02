cc0cbe3d3471e770c861f50d62a1b0fbd2777d62
Niranjan S <111201410+Ninja2k3@users.noreply.github.com>
2023-06-02 11:16:41 +0530

    register

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import Login from './components/Login';
 import Topbar from './components/Navbar';
+import Register from './components/Register';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useLocation } from 'react-router-dom'
+import { useLayoutEffect } from 'react'
+
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+} 
+
 
 function App() {
   return (
     <div className="App">
-      <Topbar/>
-      <Login/>
+      <Router>
+        <Wrapper>
+        <Topbar/>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+        </Wrapper>
+      </Router>
+
     </div>
   );
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,5 @@
 import parl from './images/parl.jpg'
-import './Login.css'
+import './Register.css'
 import {
     Card,
     Input,
@@ -8,33 +8,40 @@ import {
     Typography,
   } from "@material-tailwind/react";
    
-const Login=()=>{
+const Register=()=>{
     return(
         <div>
             <div className='login'>
       <Card className='bg-[#00000054] text-white' shadow={false}>
         <Typography className="ml-40" variant="h4" color="white" >
-          Sign In
+          Sign Up
         </Typography>
         <Typography color="white" className="mt-1 ml-24 font-normal">
-          Enter your details to sign in.
+          Enter your details to sign up.
         </Typography>
         <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
           <div className="mb-4 ml-10 flex flex-col gap-6">
             <Input size="lg" label="Aadhar No." color='white' />
+            <Input size="lg" label="Mobile No." color='white' />
             <Input type="password" size="lg" label="Password" color='white' />
+            <Input size="lg" label="Name" color='white' />
+            <Input size="lg" label="State" color='white' />
+            <Input size="lg" label="Religion" color='white' />
+            <Input size="lg" label="Profession" color='white' />
+
+
           </div>
           
           <Button className="mt-6 ml-5" fullWidth>
-            Sign in
+            Sign up
           </Button>
           <Typography color="white" className="mt-4 ml-14 text-center font-normal">
-            Don't have an account?{" "}
+            Already have an account?{" "}
             <a
-              href="/register"
+              href="/login"
               className="font-medium text-blue-500 transition-colors hover:text-blue-700"
             >
-              Register
+              Sign in
             </a>
           </Typography>
         </form>
@@ -47,5 +54,5 @@ const Login=()=>{
     )
 }
 
-export default Login
+export default Register
 
